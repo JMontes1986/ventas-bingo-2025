a47f7315a4bd41b45c92ba1dca3949cfc3f0fae7
JMontes <damon.1986@icloud.com>
2025-07-30 16:45:49 -0500

    jest.setup.ts

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,6 @@
 import '@testing-library/jest-dom';
+
+
+process.env.NEXT_PUBLIC_SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL || 'http://localhost';
+process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'anon';
+process.env.SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY || 'service';
