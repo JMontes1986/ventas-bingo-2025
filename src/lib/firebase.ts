57c0f2d8d502c4fbc9962c0f096467d92ebd5d65
JMontes <damon.1986@icloud.com>
2025-07-31 12:59:26 -0500

    firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,30 +1,18 @@
+import { initializeApp, getApp, getApps } from 'firebase/app';
+import { getPerformance } from 'firebase/performance';
 
-// Import the functions you need from the SDKs you need
-import { initializeApp, getApp, getApps } from "firebase/app";
-import { getPerformance } from "firebase/performance";
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  "projectId": "bingo-salesmate",
-  "appId": "1:5672574260:web:6befb0e6bd9670a1e4d961",
-  "storageBucket": "bingo-salesmate.appspot.com",
-  "apiKey": "AIzaSyCZqO61AEKaoz542SJV-9spx9nU4Uvvak4",
-  "authDomain": "bingo-salesmate.firebaseapp.com",
-  "measurementId": "G-YSJMFBWMXE",
-  "messagingSenderId": "5672574260"
+export const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
-let app;
-if (typeof window !== 'undefined') {
-    app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+export function initFirebase() {
+  if (typeof window === 'undefined') return;
+  const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+  getPerformance(app);
 }
-
-let performance;
-if (app && typeof window !== 'undefined') {
-    performance = getPerformance(app);
-}
-
-
-export { app, performance };
