0365361ce11b7dd57fa319c9478d58a75a324613
JMontes <damon.1986@icloud.com>
2025-08-03 20:53:44 -0500

    FirebaseInitializer.tsx

--- src/components/FirebaseInitializer.tsx
+++ src/components/FirebaseInitializer.tsx
@@ -1,13 +1,13 @@
 'use client';
 
 import { useEffect } from 'react';
-import { initializeApp } from 'firebase/app';
+import { getApps, initializeApp } from 'firebase/app';
 import { getPerformance } from 'firebase/performance';
 import { firebaseConfig } from '@/lib/firebase';
 
 export default function FirebaseInitializer() {
   useEffect(() => {
-    const app = initializeApp(firebaseConfig);
+    const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
     getPerformance(app);
   }, []);
 
